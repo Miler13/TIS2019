2a5595c6d772a2a47d075f08949f2ec88b6ea15b
realmanza <realmanza@gmail.com>
2019-04-16 00:24:11 -0400

    creacion validarAuxiliar.js
    tb modifique validarDocente.js

--- TIS/validarAuxiliar.js
+++ TIS/validarAuxiliar.js
@@ -1,12 +1,15 @@
-function validarDocente() {
-    var nombre, apellido, correo, expresion;
+function validarAuxiliar() {
+    var nombre, apellido, sis, correo, clave1, clave2, expresion;
     nombre = document.getElementById("nombre").value;
     apellido = document.getElementById("apellido").value;
+    sis = document.getElementById("sis").value;
     correo = document.getElementById("correo").value;
-    
+    clave1 = document.getElementById("clave1").value;
+    clave2 = document.getElementById("clave2").value;
+
     expresion = /\w+@\w+\.+[a-z]/;
 
-    if(nombre === "" || apellido === "" || correo === ""){
+    if(nombre === "" || apellido === "" || correo === "" || sis === "" || clave1 ==="" || clave2 ===""){
         alert("Todos los campos son obligatorios");
         return false;
     }
@@ -26,7 +29,16 @@ function validarDocente() {
         alert("El correo no es valido");
         return false;
     }
+    else if(sis.length>10){
+        alert("El codigo sis es muy largo");
+        return false;
+    }
+    else if(isNaN(sis)){
+        alert("El codigo sis ingresado no es un numero");
+        return false;
+    }
     
+
 }
 
 // esta funcion sirve para controlar el ingreso solo de letras en la caja de texto
