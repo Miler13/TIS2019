0fc656c4f40942b8ffa9b5795b2e38cf084a8686
Miler13 <milerrojassiles@gmail.com>
2019-07-15 12:44:36 -0400

    1

--- siad2/alerta.php
+++
@@ -1,8 +0,0 @@
-
-           <?php 
-           
-              echo "<script>jQuery(function(){
-                swal(\"Bienvenido al Sistema de Laboratorios de informatica-sistemas UMSS\", \"Has iniciado sesion correctamente\", \"success\");
-              });</script>";
-            
-          ?>
--- siad2/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footerBottomSection">
-        <div class="container">
-            © <a href="#"> Laboratorios de informatica-sistemas UMSS</a> 
-              <a>Plataforma es Diseño y Desarrollo de Stardust soft S.R.L Copyright © 2014-2019 . Todos los derechos reservados.<br>
-                          Stardust soft S.R.L - STARDUST SOFT SOCIEDAD DE RESPONSABILIDAD LIMITADA.<br>
-                          UMSS - Universidad Mayor de San Simon</a>
-        </div>
-    </div>
